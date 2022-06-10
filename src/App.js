a3a5801098a450c4a8133507ab58cf72777edfed
Daniel Omidiran <danielomidiran@Daniels-Air.attlocal.net>
2022-06-10 13:35:42 -0500

    tailwind-configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App bg-teal-500 h-screen flex justify-center items-center text-white">
+      Yaaaay, Tailwind works
+    </div>
+  );
 }
 
 export default App;
